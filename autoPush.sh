cd81a6cc1c273a8279be321303eef8dbc522d4eb
jake meisten <jake.meisten@gmail.com>
2023-02-01 19:22:10 -0500

    Auto Commit from shell

--- autoPush.sh
+++ autoPush.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 apiKey=$(cat ~/api.txt)
+echo 1234 > test.txt
 while [ 1 ]; do
     echo $apiKey 
+    curl -h "jmeisten:$apiKey" https://github.com/jmeisten/weddingAudioBook.git
     git add -A .
     git commit -m "Auto Commit from shell"
     git push https://github.com/jmeisten/weddingAudioBook.git
