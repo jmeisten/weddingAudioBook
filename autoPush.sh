b5dc654c3b123e903040c6b49d448526ec562640
jmeisten <jake.meisten@gmail.com>
2023-02-01 19:53:04 -0500

    Auto Commit from shell

--- autoPush.sh
+++ autoPush.sh
@@ -6,6 +6,6 @@ while [ 1 ]; do
     echo $apiKey 
     git add -A .
     git commit -m "Auto Commit from shell"
-    git push https://github.com/jmeisten/weddingAudioBook.git
+    git push https://$apiKey@github.com/jmeisten/weddingAudioBook.git
     sleep 15
 done
