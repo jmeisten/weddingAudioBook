fe0a7e9c24cf43e4296bbf42749867e2578f96e2
jmeisten <jake.meisten@gmail.com>
2023-02-01 20:02:07 -0500

    Auto Commit from shell

--- autoPush.sh
+++ autoPush.sh
@@ -2,10 +2,21 @@
 
 apiKey=$(cat ~/api.txt)
 echo 1234 > test.txt
+
 while [ 1 ]; do
-    echo $apiKey 
-    git add -A .
-    git commit -m "Auto Commit from shell"
-    git push https://$apiKey@github.com/jmeisten/weddingAudioBook.git
-    sleep 15
+    online=$(curl -s -o /dev/null -w "%{http_code}" http://www.google.com)
+    echo $online
+    if [ $online == 200 ]
+    then
+        git add -A .
+        git commit -m "Auto Commit from shell"
+        git push https://$apiKey@github.com/jmeisten/weddingAudioBook.git
+        echo "Git pushed successfully"
+        sleep 15m
+    else
+        echo "Not online"
+    fi
+
+
+    
 done
